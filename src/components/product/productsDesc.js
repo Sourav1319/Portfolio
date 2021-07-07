7fe89f795b36454c43d56adfc1aee58cc9a8c64e
Sourav1319 <66901293+Sourav1319@users.noreply.github.com>
2021-07-07 17:37:37 +0530

    Update productsDesc.js

--- src/components/product/productsDesc.js
+++ src/components/product/productsDesc.js
@@ -10,7 +10,7 @@ export const products = [
     colorCode: "rgb(255, 122, 0)",
   },
   {
-    title: "Ecommerce Platform with CRUD functionality and Production grade authentication System",
+    title: "Ecommerce Platform with CRUD functionality and Authentication system",
     about:
       "Users can browse and add,remove or update items to their shopping cart while admins will have their own separate page where they will know what products they are selling on the site with CRUD funtionality.Database Handling is done from scratch, no external applicaton is used. Built using Node.js and Bulma Css Framework.Express-validator is used to handle validation.",
     link: "https://thawing-escarpment-54673.herokuapp.com/",
@@ -20,10 +20,9 @@ export const products = [
     colorCode: "rgb(250, 215, 90)",
   },
   {
-    title: "YelpCamp",
+    title: "Evermore",
     about: 
-    "YelpCamp is a website where users can create and review campgrounds. In order to review or create a campground, you must have an account. This project was part of Colt Steele's web dev course on udemy.This project was created using Node.js, Express, MongoDB, and Bootstrap. Passport.js was used to handle authentication.",
-
+    "A Platform where students can either sell or buy book from their seniors/juniors in college. This project was created using Node.js, Express, MongoDB, and Bootstrap, Passport.js was used to handle authentication.",
     link: "https://stormy-temple-11706.herokuapp.com/",
     source: "https://github.com/Sourav1319/Yelpcamp",
     username: "Sourav1319",
